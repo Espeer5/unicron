42030ac58c535b0e1d4c2b52e35bfaea0a739a61
Team Unicron <robot@unicron.local>
2023-04-09 15:39:18 -0700

    create main script for demo1

---
+++ demo1.py
@@ -0,0 +1,9 @@
+# This file contains the first weekly demo for CS/ME/EE 129 for team unicron
+# The demo drives the robot in a square, executing a point turn at each corner
+# This file requires the utility functions in driveUtilities.py
+
+# Authors: Edward Speer, Garrett Knuf
+# Date: 4/9/23
+
+if __name__ == "__main__":
+    #main loop
