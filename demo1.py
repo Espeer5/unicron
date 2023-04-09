52121021242ce4b2848b2175038f2922698a984b
Team Unicron <robot@unicron.local>
2023-04-09 16:05:59 -0700

    update motor utilities and demo1

--- demo1.py
+++ demo1.py
@@ -5,5 +5,32 @@
 # Authors: Edward Speer, Garrett Knuf
 # Date: 4/9/23
 
+#imports
+import time
+
+TOP_SPEED = #TODO
+ACCEL_DURATION = #TODO
+DRIVE_DUR = #TODO
+
 if __name__ == "__main__":
-    #main loop
+    
+    # Create a state variable for the current linear speed of the robot
+    current_speed = 0
+
+    # Prepare GPIO Interface, prepare PWM, and clear all pins
+    driveSetup()
+
+    #For each of the 4 sides of the square, drive forward, then rotate by 90 degrees
+    for i in range(4):
+
+        # Accelerate up to speed in the forward direction
+        current_speed = accelerate(current_speed, TOP_SPEED, ACCEL_DURATION)
+        
+        # Drive forward for the drive duration
+        time.sleep(DRIVE_DUR)
+
+        # Deccelerate down to a stop
+        current_speed = deccelerate(current_speed, )
+
+        # Execute a 90 degree clockwise turn
+
