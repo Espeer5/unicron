ba9415ef564c2f2d3a78a8f90589f5bfd4636452
Edward Earl Speer <espeer@caltech.edu>
2023-04-10 03:22:08 +0000

    remove redundant directional functions in driveUtilities and add docstrings

--- driveUtilities.py
+++ driveUtilities.py
@@ -1,5 +1,5 @@
 """ This file contains basic drive train functions without sensors 
-    Delays will be used to control duration of function calls"""
+    Delays will be used to control duration of function calls """
 
 # Authors: Edward Speer, Garrett Knuf
 # Date: 4/9/23
@@ -17,26 +17,28 @@ R_MOTOR_PINB = 6
 # Motor control settings
 PWM_FREQ = 1000
 
-def driveForward(left_motor, right_motor, speed, duration):
+
+def driveStraight(left_motor, right_motor, speed, duration):
+    """
+      Drives the robot in a straight line at the given speed for the given duration
+    """
     left_motor.setSpeed(speed)
     right_motor.setSpeed(-(speed+18))
     time.sleep(duration)
     
-def driveBackward(left_motor, right_motor, speed, duration):
-    left_motor.setSpeed(-speed)
-    right_motor.setSpeed(speed)
-    time.sleep(duration)
-    
-def turnLeft(left_motor, right_motor, speed, duration):
-    left_motor.setSpeed(-speed)
-    right_motor.setSpeed(-speed)
-    time.sleep(duration)
-    
-def turnRight(left_motor, right_motor, speed, duration):
+
+def rotate(left_motor, right_motor, speed, duration):
+    """
+      Rotates the robot in place by performing a point turn
+    """
     left_motor.setSpeed(speed)
     right_motor.setSpeed(speed)
     time.sleep(duration)
     
+
 def disableDrive(left_motor, right_motor):
+    """
+      Immediately stops all motor activity for the robot
+    """
     left_motor.disable()
     right_motor.disable()
