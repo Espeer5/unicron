6c7d638f95d56d825c5954d583f46db6322b8946
espeer <espeer@caltech.edu>
2023-06-03 12:00:13 -0700

    Add gitignore

---
+++ demo.py
@@ -0,0 +1,14 @@
+"""This file contains the main loop which executes the robot code according to 
+the user interface defined in ui.py. This demo loop simply executes the UI 
+which accepts user input to determine the next behavior of the bot.
+"""
+
+# Authors: Edward Speer, Garrett Knuf
+# Date: 6/3/23
+
+from ui import ui_cmp
+
+
+if __name__ == "__main__":
+    ui_cmp()
+    
